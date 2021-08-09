fd6f500292081742e790f72dff4e5599a67459ab
Giovani Silva <giovani-silva1@live.com>
2021-08-09 20:48:18 -0300

    criacao do projeto

---
+++ src/main/java/br/com/helpdesk/HelpdeskBeApplication.java
@@ -0,0 +1,13 @@
+package br.com.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskBeApplication.class, args);
+	}
+
+}
